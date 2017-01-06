6e0e864e748d07a7e84a67e45df5686e66c06a17
jenniferm4926 <jenniferm4926@hstat.org>
2017-01-06 13:34:21 +0000

    hw

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+    attr_accessor :roster
+  def initialize(school_name)
+        @@school_name = school_name
+        @roster = {} #hash
+  end
+    def add_student(name,grade)
+      unless @roster[grade] != nil
+        @roster[grade] = [] #[] putting into an array 
+      end
+      @roster[grade] << name
+    end
+    
+    def grade(num)
+      @roster[num]
+    end
+    def sort
+      @roster.each do |grade,name|
+        #   homophones_sorted[number] = words.sort 
+       @roster[grade] = name.sort
+      end
+    end
+end
