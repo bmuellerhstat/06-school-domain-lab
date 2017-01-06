378f72768e2335ae2766fdedca4e5335608cf79f
ziqianp0918 <ziqianp0918@hstat.org>
2017-01-06 00:47:36 +0000

    U can't use school_roster.....

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+#https://repl.it/FAda
+
+class School
+    attr_accessor :name  
+    attr_reader :roster
+ 
+    
+    def initialize(school_name)
+        @school = school_name
+       @roster = {}
+       
+end
+
+
+def add_student(name, grade)
+    @name = name
+    @grade = grade
+ unless @roster[grade] != nil
+ @roster[grade] = []
+end
+
+@roster[grade] << name
+ end
+
+def grade(grade_number)
+    
+@roster[grade_number]
+    
+end
+
+def sort
+   @roster.each {|grade, name | name.sort!}
+    
+end
+
+end
+
