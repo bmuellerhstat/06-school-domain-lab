cc3018879fb19c82baaba0db545202c771ee1e2f
ryans4707 <ryans4707@hstat.org>
2017-01-06 04:59:50 +0000

    could not finish

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+    @student
+end
+def add_student(student_name,grade)
+#In the roster, the grade key should point to a value of an empty array unless it already exists
+ @roster[grade] = []
+ @roster[grade] << student_name
+end
+
+def grade
+ @roster[number]
+end
+school.new("HSTAT")
+
+
+#
+#
+#
+#
+end
