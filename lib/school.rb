0ae0a91f43c44beccfe9eeb0cfdd320fc66bc5fd
Jun <junh4533@hstat.org>
2017-01-06 03:24:38 +0000

    hw

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+    attr_accessor :school_name
+    attr_accessor :roster
+    
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+    
+    def add_student(name, grade)
+        
+        if @roster[grade] == nil #if the grade level doesn't exist
+            @roster[grade] = [] #make a new grade equal to an empty array
+            @roster[grade] << name #push name into grade
+        else #otherwise, the grade level does exist
+            @roster[grade] << name #simply push name into grade
+        end
+
+    end
+ 
+    def grade(selected_grade)
+        @roster.each do |grade, name|
+          return name if selected_grade == grade
+        end
+    end
+    
+    def sort
+        @roster.each do |grade, names|
+            names.sort
+        end
+        @roster.sort
+    end
+    
+end
