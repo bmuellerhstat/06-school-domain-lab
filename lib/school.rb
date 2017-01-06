5d518d91b375d28f4e56014f6fe0e4b8a54fa546
christianf9156 <christianf9156@hstat.org>
2017-01-06 17:08:53 +0000

    Need help with hw

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School 
+   def initialize(roster)
+       @roster = {}
+   end
+    
+    def roster
+        @roster
+    end
+    
+    def add_student(name,grade)
+        @name = name
+        @grade = grade
+        arr = []
+        
+        @roster[@grade] =  arr.push(@name)
+     
+        
+       
+    end
+    
+    def name
+        @name
+    end
+    
+    def grade
+        @grade
+    end
+end
