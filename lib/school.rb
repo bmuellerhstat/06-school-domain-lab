90af9deda41f3ad6c71b61502269100f780d2e22
derekc4859 <derekc4859@hstat.org>
2017-01-06 00:10:13 +0000

    Finish School Domain Lab!

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+    attr_reader :roster
+    attr_accessor :student_name
+    
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def add_student(student_name,grade)
+        unless @roster[grade] != nil
+        @roster[grade] = []
+        end
+        @roster[grade] << student_name
+    end
+    
+    def grade(number)
+      @roster[number]
+    end
+    
+    def sort
+      @roster.each do |grade,student_names|
+          student_names.sort!
+      end
+      @roster
+    end
+end
