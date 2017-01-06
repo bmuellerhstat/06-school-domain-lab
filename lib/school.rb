f44566a0314eaaa616b1a3da354f2dffa82d3a70
Jenny Feng <jennyfeng.ga.ling1999@gmail.com>
2017-01-06 16:51:25 +0000

    attempt add_student

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+class School
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+    attr_accessor :roster
+    
+    # roster = {9 => [], 10 => [], 11 => [], 12 => []}
+    
+    def add_student(student_name,grade)
+        #grade key should point to a value an empty array unless it already exists (isn't *nil*)
+        unless @roster[grade] != nil 
+            @roster[grade] = []
+        end
+        #shovel in the student's name into the grade
+        @roster[grade] << student_name
+    end
+end
