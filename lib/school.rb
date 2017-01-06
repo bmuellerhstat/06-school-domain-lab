ef903fed9610c0a498f1209b53ae3d2632aa44d0
jeremye3180 <Jeremye3180@hstat.org>
2017-01-06 11:29:45 +0000

    Finish lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School
+
+ attr_accessor :name
+ 
+ attr_accessor :roster
+ 
+def initialize(name)
+ @name = name 
+ @roster = {}
+end
+
+def add_student(student_name, grade)
+ unless @roster[grade] != nil 
+ @roster[grade] = []
+ end
+ @roster[grade] << student_name
+end
+
+def grade(level) 
+ @roster[level]
+end
+
+def sort
+ @roster.each do |grade,student_name|
+  student_name.sort!
+end
+end
+end
