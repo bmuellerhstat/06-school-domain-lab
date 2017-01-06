68da97ea55e2e07a11bc0bed1f288bb828e320c4
meitingi2005 <meitingi2005@hstat.org>
2017-01-06 06:12:25 +0000

    DONE

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+    attr_accessor :roster
+    def initialize(school_name)
+        @school_name=school_name
+        @roster={}
+    end
+    
+    def add_student(name, grade)
+        unless @roster[grade]!=nil
+        @roster[grade] = [] 
+    end
+        @roster[grade] << name
+    
+    end
+    
+    def grade(student_grade)
+        @student_grade = student_grade
+        @student_grade =  @roster[student_grade]
+    end
+    
+    def sort
+        @roster.each do |grade,names|
+        @roster[grade]=names.sort
+    end
+    end
+end
