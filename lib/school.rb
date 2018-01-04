1b9e0772d636ae5e05bd217d1c49b1ec1f735d9d
zeled3813 <zeled3813@hstat.org>
2018-01-04 22:40:54 +0000

    rspec done-had to return cuz .each

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+    attr_accessor :roster
+    
+   def initialize(school_name)
+       @school = school_name
+       @roster = {}
+       
+   end
+   
+   def add_student(name,grade)
+    if @roster[grade] == nil
+        @roster[grade] = []
+        @roster[grade] << name
+    else
+        @roster[grade] << name
+    end
+   end
+   
+   def grade(grade)
+       @roster[grade]
+   end
+   
+   def sort
+       roster_sorted = {}
+       roster.sort.each do |grade, names|
+           roster_sorted[grade] = names.sort
+       end
+      roster_sorted
+   end 
+end
+
+School.new("HSTAT")
