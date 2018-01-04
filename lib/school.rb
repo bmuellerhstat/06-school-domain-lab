f178bff6bcd8ce0880aac76316509d859e7c22d0
Kelly Lai <zhiyinl5633@hstat.org>
2018-01-04 01:09:08 +0000

    sort students

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    attr_accessor :roster
+  def initialize(name)
+      @name = name
+      @roster = {}
+  end
+  
+  def add_student(name,grade)
+       unless  @roster[grade] != nil #grade key doesn't exist
+            @roster[grade] = []
+       end
+       @roster[grade] << name
+  end
+  
+  def grade(grade)
+      @roster[grade]
+  end
+  
+  def sort
+      @roster.each do |grade,students|
+          @roster[grade] = students.sort #abc order
+      end
+  end
+end
