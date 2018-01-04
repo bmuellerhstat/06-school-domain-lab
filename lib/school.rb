5dcd0b2db25b234644d2d1ff22a0553d19131bb3
geoffreyl4412 <geoffreyl4412@hstat.org>
2018-01-04 01:38:45 +0000

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+    attr_accessor :roster
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    
+    def add_student(name, grade)
+        if roster[grade].nil?
+            roster[grade] = []
+            roster[grade] << name
+        else
+            roster[grade] << name
+        end
+    end
+    
+    def grade(grade_level)
+        roster[grade_level]
+    end
+    
+    def sort
+        roster.each do |grade, name|
+            roster[grade] = name.sort
+        end
+    end
+end
