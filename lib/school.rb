a07510b2be7764837c41eeaf764195edd8b80b85
Xavier Jiang <xavierj2216@hstat.org>
2018-12-15 23:23:08 +0000

    finish school lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School 
+    def initialize(school_name)
+        @roster = {}
+    end
+    
+    def roster
+        @roster
+    end
+    
+    def add_student(student_name,grade)
+        if @roster[grade].nil?
+            @roster[grade] = []
+            @roster[grade] << student_name
+        elsif
+            @roster[grade] << student_name
+        end
+    end
+    
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+        student_sorted = {}
+        @roster.sort.each do |num, student|
+            student_sorted[num] = student.sort
+        end
+        return student_sorted
+    end
+end
