41e3045284c13c1b1f0bae9c77bcdcddac8749e7
Shi Wei Zheng <shiz3150@hstat.org>
2018-12-15 22:02:13 +0000

    resolve all errors

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+    def initialize(name)
+        @roster = {}
+        def roster
+            @roster 
+        end
+    end
+    
+    def add_student(name, grade)
+        if @roster[grade].nil?
+            @roster[grade] = []
+            @roster[grade] << name
+        else
+            @roster[grade] << name
+        end
+    end
+    
+    def grade(num)
+        @roster[num]
+    end
+    
+    def sort
+        name_sort = {}
+        @roster.sort.to_h.each do |grade, names|
+            name_sort[grade] = names.sort
+        end
+        return name_sort
+    end
+end
