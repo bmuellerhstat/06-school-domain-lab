24cd418f26d04e9011a8d0676d600ec663c49c40
Xiurong Yu <xiurongy3506@hstat.org>
2018-12-16 20:41:41 +0000

    create domain model for a school

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+    def initialize(school_name)
+       @school_name = school_name
+       @roster = {}
+    end
+    
+    def roster
+        @roster
+    end 
+    
+    def add_student(name, grade)
+        if @roster[grade].nil?
+            @roster[grade] = []
+        end
+        @roster[grade] << name
+    end
+    
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+        roster_sorted = {}
+        @roster.each do |grade, name|
+            roster_sorted[grade] = name.sort
+        end
+        roster_sorted
+    end
+    
+end
