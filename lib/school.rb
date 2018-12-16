cf1218ad14b843dba48f8b53ce1bbadaed273e9a
JENNIFER LIU <jenniferl4209@hstat.org>
2018-12-16 19:47:31 +0000

    schoooooool

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+    attr_accessor :roster
+    def initialize(school_name)
+        @roster = {}
+        def roster
+            @roster 
+        end
+    end
+    
+    def add_student(name,grade)
+        if @roster[grade].nil?
+            @roster[grade] = [] #creates an empty array to add names
+            @roster[grade] << name
+        else
+            @roster[grade] << name
+        end
+    end
+   
+   def grade(grade)
+       @roster[grade]
+   end
+   
+   def sort
+       @roster.each do |grades,names|
+           @roster[grades] = names.sort
+       end
+   end
+end
