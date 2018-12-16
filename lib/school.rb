d4181518a4b1151d523d08af930fb37a6abb6858
Arieta Haskaj <arietah8545@hstat.org>
2018-12-16 19:55:34 +0000

    pass rspec

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    
+    attr_accessor :roster
+    
+    def add_student(name, grade)
+        if @roster.keys.include?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+    
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+        @roster_sorted = {}
+        @roster.each do |grade, students_arr|
+            @roster_sorted[grade] = students_arr.sort
+        end
+        @roster_sorted
+    end
+end
