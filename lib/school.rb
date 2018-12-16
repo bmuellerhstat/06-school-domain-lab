2ea19a8cd3d0c81a669edb646d7b3093597560d0
Sherzod <sherzodm8441@hstat.org>
2018-12-16 06:44:04 +0000

    ugh its school again

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,33 @@
 # code here!
+
 class School
-   def initialize(school_name)
-      @name = school_name 
+   attr_accessor :roster
+   def initialize(name)
+      
+      @name = name
+      @roster = {}
+      
+   end
+   
+   def add_student(student_name, grade_level)
+      if @roster[grade_level] == nil
+         @roster[grade_level] = []
+         @roster[grade_level] << student_name
+      else
+         @roster[grade_level] << student_name
+      end
+   end
+   
+   def grade(grade_level)
+      @roster[grade_level]
+   end
+   
+   def sort
+      roster_sorted = {}
+      @roster.each { |grade, student|
+      roster_sorted[grade] = student.sort
+      
+      }
+      return roster_sorted
    end
 end
