6c32ff4deb609ef19dcd0887c17a39088819568c
Victoria Feng <victoriaf6656@hstat.org>
2018-12-16 22:44:42 +0000

    finish school.rb file

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+    def roster
+        @roster
+    end
+    def add_student(student_name,grade)
+        if @roster[grade] == nil
+           @roster[grade] = []
+        end
+        @roster[grade] << student_name
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        sorted_students = {}
+        @roster.each do |grade,student_name|
+            sorted_students[grade] = student_name.sort
+        end
+        sorted_students
+    end
+end
