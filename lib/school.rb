39f3ec5cfb87272ce3fc1b515bd5f8b37df9332b
Toby lau <yatchol6214@hstat.org>
2018-12-17 06:19:28 +0000

    a

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School
+    
+    
+    def initialize(school_name)
+        @roster = {}
+        
+    end
+    
+    def roster
+        @roster
+    end
+    
+    def add_student(name,grade)
+        
+        if @roster[grade].nil?
+            @roster[grade] = []
+        end    
+        @roster[grade] << name
+    
+    end    
+    
+    def grade(grade)
+        @roster[grade]
+    end    
+    
+    def sort
+        sorted_student ={}
+            @roster.map do |grade,student|
+                sorted_student[grade] = student.sort
+            end
+        sorted_student
+    end
+
+end
