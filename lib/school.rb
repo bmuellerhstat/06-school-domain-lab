149e9ea79ba4f11a530c51223d7494d02db7db28
Raquel Joseph <raquelj6437@hstat.org>
2018-12-17 14:51:17 +0000

    pass test

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+    def initialize(name, roster = {})
+        @name = name
+        @roster = roster
+    end
+    
+    def roster
+        @roster
+    end
+    
+    def add_student(student,grade)
+        if @roster.key?(grade)
+            @roster[grade] << student
+        else
+            @roster[grade]=[student]
+        end
+    end
+    
+    def grade(cohort)
+        @roster[cohort]
+    end
+    
+    def sort
+        sorted_list = {}
+        @roster.each do |grade,student|
+            sorted_list[grade]=student.sort
+        end
+        @roster = sorted_list
+    end
+end
