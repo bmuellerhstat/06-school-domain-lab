4d11a54825561dc38452a536f942277d6902b92f
Ahmad Rabah <ahmadr9279@hstat.org>
2018-12-17 03:53:52 +0000

    fix errors

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+  def roster=(roster)
+    @roster=roster
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def initialize(school)
+    @school=school
+    @roster = {}#empty roster when initialized
+  end
+  
+  def add_student(student, grade)
+    if @roster[grade].nil?
+      @roster[grade]= []
+    end
+    @roster[grade] << student#adds student to a grade
+  end
+
+  def grade(grade)
+    @roster[grade]#retrieves student from grade
+  end
+  
+  def sort
+    @roster.each do |grade, student|
+      @roster[grade]=student.sort
+    end
+  end
+  @roster
+end
+@school = School.new("Test School")
