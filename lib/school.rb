bf5751431ac264f910e71c3eec212d7eb6d218d3
Kasper Gacek <kasperg3971@hstat.org>
2018-12-17 04:20:54 +0000

    Time for school

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def roster
+        @roster
+    end
+    def add_student(student_name,grade)
+        if @roster[grade] == nil
+            @roster[grade] = []
+        end
+            @roster[grade] << student_name
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        students_sorted = {}
+        @roster.each do |grade,student_name|
+            students_sorted[grade] =student_name.sort
+        end
+        students_sorted
+    end
+end
