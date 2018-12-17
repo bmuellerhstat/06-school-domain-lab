c9fa9acf2b7a6f077c1ca52b24135a85661d3d6a
Travis Matos <travism7618@hstat.org>
2018-12-17 17:07:16 +0000

    all but one test

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    
+    def roster
+        @roster
+    end
+    
+
+    def add_student(student,grade)
+        if @roster[grade].nil?
+            @roster[grade]=[]
+            @roster[grade] << student 
+        else
+            @roster[grade] << student 
+        end
+    end
+    
+    def grade(num)
+        @roster[num]
+    end
+end
